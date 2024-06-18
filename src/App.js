97769b24be06e5c952cc6d1d40025bd5e50475b6
Pruthvi <pruthvi.robo@gmail.com>
2024-06-18 23:19:45 +0530

    Day 2 . 18-06-2024 , Done till dark mode , video 10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+// import Textform from './components/Textform';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar title="TextUtils" />
       <div className="container">
-        <Textform heading="Enter the text to analyze" />
+        {/* <Textform heading="Enter the text to analyze" /> */}
+      <About/>
       </div>
     </>
   );
