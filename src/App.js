912a900849758072c44fca14340da2e50693acbe
Pruthvi <pruthvi.robo@gmail.com>
2024-06-19 15:50:04 +0530

    Added a Dark mode Changing Button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import About from './components/About';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
-// import Textform from './components/Textform';
+import Textform from './components/Textform';
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = ()=>{
+    if (mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode ={toggleMode}/>
       <div className="container">
-        {/* <Textform heading="Enter the text to analyze" /> */}
-      <About/>
+        <Textform heading="Enter the text to analyze" mode={mode}/>
+      {/* <About/> */}
       </div>
     </>
   );
 }
-
 export default App;
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -23,15 +23,15 @@ export default function Textform(props) {
     const [text, setText] = useState("");
     return (
         <>
-        <div className='container'> 
+        <div className='container'  style={{color: props.mode==='light' ? '#042743':'white'}}> 
             <h2>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'gray':'white', color:props.mode==='light' ? '#042743':'white' }} id="myBox" rows="8" ></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upperCase</button>
             <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
         </div>
-        <div className="container my-4"  >
+        <div className="container my-4"  style={{color:props.mode==='light' ? '#042743':'white'}}>
             <h2>Text Sumarry</h2>
             <div>{text.split(" ").length} Words and {text.length} Characters</div>
         </div>
