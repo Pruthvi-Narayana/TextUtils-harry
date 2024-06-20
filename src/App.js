3128257239fd4458be749f6fa4dbea41359902eb
Pruthvi <pruthvi.robo@gmail.com>
2024-06-20 15:31:35 +0530

    Routing added to Navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import './App.css';
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+import "./App.css";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import About from "./components/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
-  const toggleMode = ()=>{
-    if (mode === "light"){
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-    }
-    else{
-      setMode('light');
+    } else {
+      setMode("light");
       document.body.style.backgroundColor = "white";
     }
-  }
+  };
 
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode ={toggleMode}/>
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+
       <div className="container">
-        <Textform heading="Enter the text to analyze" mode={mode}/>
-      {/* <About/> */}
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Textform heading="Enter the text to analyze" mode={mode} />} />
+        </Routes>
       </div>
+    </Router>
     </>
   );
 }
